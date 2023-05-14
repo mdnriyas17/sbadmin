080510ab100031a18a52cd919201cd766e4f90e2
mdnriyas17 <mdnriyas@gmail.com>
2023-05-14 09:27:56 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';  
+import './styles/sb-admin-2.min.css';
+import './App.css';
+import Modal from './pages/modal';
+
+
+
+function App() {
+  return(
+  <div className="App">  
+  <Modal/>
+        
+  </div>
+);
+}
+
+export default App;
